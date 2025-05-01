df237638dbb9271950f600997fb0400214c99448
Yann <yann.duchateau97@gmail.com>
2025-05-01 23:58:32 +0200

    tri

--- Revision.js
+++ Revision.js
@@ -88,3 +88,33 @@ function deleteDuplicate(){
 
 
 // ------------------------------------- sortwithoutsortmethod ---------------------------------------------------
+
+
+let randomArray = [12, 32, 1, 34, 23, 35, 1, 50]
+
+function sort(array){
+    
+    for(let j = 0; j < array.length; j++){
+
+        for(let i = 0; i < array.length -1; i++){
+
+            console.log(array[i], array[i +1])
+
+            if(array[i] > array[i +1]){
+                let temp = array[i]
+                array[i] = array[i +1]
+                array[i +1] = temp;
+            }
+
+            
+        } 
+
+        console.log(array)
+
+
+    }
+    
+
+    }
+
+sort(randomArray)
